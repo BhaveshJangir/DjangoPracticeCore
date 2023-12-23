dd3cfc30cdc86361d9ad89c406f5635e2392eb74
Bhavesh Jangir <bjangir266@gmail.com>
2023-12-23 21:40:13 +0530

    Migrations and migrate example (db)

--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    # id = models.AutoField()
+    name = models.CharField(max_length = 100)
+    age  = models.IntegerField()
+    email = models.EmailField()
+    image = models.ImageField(null=True, blank=True)
+    file = models.FileField()
+    
+class product(models.Model):
+    pass
